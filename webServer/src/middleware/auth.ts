a34d26974d124875f686e7f38839f2071be72cd7
JaydenShel <jshelderfer@ufl.edu>
2025-10-20 18:44:15 -0400

    format2

--- webServer/src/middleware/auth.ts
+++ webServer/src/middleware/auth.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 import { JwtUser } from '../types/express';
 
 // Extend Express Request to include user
